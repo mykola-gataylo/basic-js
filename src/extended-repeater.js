84870bcb58bffd21e5f78a4fdee4d4a1b17d6d9a
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-09-23 11:03:34 +0200

    feat: feat solution for extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,60 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let stringArray = [];
+  let stringArray2 = [];
+  let result = "";
+  let indexOfEl = 1;
+
+  if (!options.repeatTimes) {
+    result = `${str}${options.addition}`;
+    console.log(result);
+    return result;
+  }
+
+  for (let i = 1; i <= options.additionRepeatTimes; i++) {
+    if (options.addition) {
+      stringArray2.push(options.addition);
+    }
+
+    if (options.additionSeparator) {
+      stringArray2.push(options.additionSeparator);
+    }
+  }
+
+  let stringArray2Length = stringArray2.length;
+
+  stringArray2.splice(stringArray2Length - 1, 1);
+
+  stringArray2.unshift(str);
+  if (!options.separator) {
+    options.separator = "+";
+  }
+
+  stringArray2.push(options.separator);
+  let length = options.separator.length;
+
+  result = stringArray2.join("").repeat(options.repeatTimes);
+  let lengthResult = result.length;
+  let arrayOfResult = result.split("");
+  arrayOfResult.splice(lengthResult - length, length);
+
+  console.log(arrayOfResult.join(""));
+
+  return arrayOfResult.join("");
 }
